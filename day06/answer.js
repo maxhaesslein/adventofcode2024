ec2c65f14522449a57dc56f18b650d8e904a65f6
maxhaesslein <git@maxhaesslein.de>
2024-12-06 12:08:23 +0100

    day 06 part 2

--- day06/answer.js
+++ day06/answer.js
@@ -14,19 +14,10 @@ function answer1( input ) {
 
 	var map = input;
 
-	var curX, curY;
-	for( var y = 0; y < map.length; y++ ) {
-		for( var x = 0; x < map[y].length; x++ ) {
+	var start = getStartPosition(map);
 
-			if( map[y][x] != '^' ) continue;
-
-			curX = x,
-			curY = y;
-
-			break;
-
-		}
-	}
+	var curX = start[0],
+		curY = start[1];
 
 	map[curY][curX] = 'X';
 
@@ -83,7 +74,101 @@ function answer1( input ) {
 }
 
 
+function getStartPosition( map ) {
+
+	for( var y = 0; y < map.length; y++ ) {
+		for( var x = 0; x < map[y].length; x++ ) {
+
+			if( map[y][x] != '^' ) continue;
+
+			return [x, y];
+		}
+	}
+
+	return false;
+}
+
+
 function answer2( input ) {
 
-	return "?";
+	var map = input;
+
+	var start = getStartPosition(map);
+
+	var sum = 0;
+
+	for( var y = 0; y < map.length; y++ ) {
+		for( var x = 0; x < map[y].length; x++ ) {
+
+			console.log('testing', x+'/'+(map[y].length-1), y+'/'+(map.length-1) );
+
+			if( loop(x, y, start, structuredClone(map)) ) {
+				sum++;
+			}
+
+		}
+	}
+
+	return sum;
+}
+
+
+function loop( obstacleX, obstacleY, start, map ) {
+
+	if( map[obstacleY][obstacleX] === '#' ) {
+		return false;
+	} else if( map[obstacleY][obstacleX] === '^' ) {
+		return false;
+	}
+
+	var curX = start[0],
+		curY = start[1];
+
+	var dir = 0; // 0: top, 1: right, 2: down, 3: left
+
+	map[curY][curX] = dir;
+
+	map[obstacleY][obstacleX] = '#'; // place obstacle
+
+	while( true ) {
+
+		var x = curX,
+			y = curY;
+
+		if( dir === 0 ) {
+			y--;
+		} else if( dir === 1 ) {
+			x++;
+		} else if( dir === 2 ) {
+			y++;
+		} else if( dir === 3 ) {
+			x--;
+		}
+
+		if( y < 0 || y >= map.length ) {
+			break;
+		} else if( x < 0 || x >= map[y].length ) {
+			break;
+		}
+
+		if( map[y][x] === '#' ) {
+			dir++;
+			if( dir >= 4 ) dir = 0;
+
+			x = curX;
+			y = curY;
+		} else if( Array.isArray(map[y][x]) && map[y][x].includes(dir) ) {
+			// loop detected, we visited this map tile with this direction before!
+			return true;
+		}
+
+		if( ! Array.isArray(map[y][x]) ) map[y][x] = [];
+		map[y][x].push(dir);
+
+		curX = x;
+		curY = y;
+
+	}
+
+	return false;
 }
