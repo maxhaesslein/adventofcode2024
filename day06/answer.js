553817fdc18601755972c9231d005badfc40011a
maxhaesslein <git@maxhaesslein.de>
2024-12-07 08:45:53 +0100

    day 06 part 2: more speedup (from ~8s to ~1s )

--- day06/answer.js
+++ day06/answer.js
@@ -12,16 +12,91 @@ function prepareInput( input ) {
 
 function answer1( input ) {
 
-	var map = input;
+	map = getPath( input );
+
+	var sum = 0;
+
+	for( var y = 0; y < map.length; y++ ) {
+		for( var x = 0; x < map[y].length; x++ ) {
+			if( map[y][x] === undefined ) continue;
+			
+			sum++;
+		}
+	}
+
+	return sum;
+}
+
+
+function answer2( map ) {
+
+	// we only need to place obstacles on the original path the guard took, no need to place an obstacle where the guard will never be
+	var originalPath = getPath( map );
+
+	var sum = 0;
+
+	for( var y = 0; y < originalPath.length; y++ ) {
+		for( var x = 0; x < originalPath[y].length; x++ ) {
+
+			if( originalPath[y][x] === undefined ) continue;
+
+			// no need to simulate all the steps that lead up to this obstacle, start at the previous position right before the new obstacle
+			var originalDirection = originalPath[y][x],
+				start = [x,y];
+			if( originalDirection === 0 ) {
+				start[1]++;
+			} else if( originalDirection === 1 ) {
+				start[0]--;
+			} else if( originalDirection === 2 ) {
+				start[1]--;
+			} else if( originalDirection === 3 ) {
+				start[0]++;
+			}
+
+			if( loop(x, y, start, map, originalDirection) ) {
+				sum++;
+			}
+
+		}
+		
+		if( (y+9)%10 === 0 ) {
+			console.log('testing', (y+9)+'/'+originalPath.length );
+		}
+
+	}
+
+	return sum;
+}
+
+
+function getStartPosition( map ) {
+
+	for( var y = 0; y < map.length; y++ ) {
+		for( var x = 0; x < map[y].length; x++ ) {
+
+			if( map[y][x] != '^' ) continue;
+
+			return [x, y];
+		}
+	}
+
+	return false;
+}
+
+
+function getPath( map ) {
 
 	var start = getStartPosition(map);
 
+	var visitedMap = Array.from({ length: map.length }, function(){
+		return Array(map[0].length);
+	});
+
 	var curX = start[0],
-		curY = start[1];
+		curY = start[1],
+		dir = 0; // 0: top, 1: right, 2: down, 3: left
 
-	map[curY][curX] = 'X';
-
-	var dir = 0; // 0: top, 1: right, 2: down, 3: left
+	visitedMap[curY][curX] = dir;
 
 	while( true ) {
 
@@ -52,80 +127,21 @@ function answer1( input ) {
 			y = curY;
 		}
 
-		map[y][x] = 'X';
+		if( visitedMap[y][x] === undefined ) visitedMap[y][x] = dir; // record the direction we entered this tile with, but only the first time we encounter it
 
 		curX = x;
 		curY = y;
 
 	}
 
-
-	var sum = 0;
-
-	for( var y = 0; y < map.length; y++ ) {
-		for( var x = 0; x < map[y].length; x++ ) {
-			if( map[y][x] === 'X' ) {
-				sum++;
-			}
-		}
-	}
-
-	return sum;
+	return visitedMap;
 }
 
 
-function getStartPosition( map ) {
-
-	for( var y = 0; y < map.length; y++ ) {
-		for( var x = 0; x < map[y].length; x++ ) {
-
-			if( map[y][x] != '^' ) continue;
-
-			return [x, y];
-		}
-	}
-
-	return false;
-}
-
-
-function answer2( input ) {
-
-	var map = input;
-
-	var start = getStartPosition(map);
-
-	var sum = 0;
-
-	for( var y = 0; y < map.length; y++ ) {
-		for( var x = 0; x < map[y].length; x++ ) {
-
-			if( map[y][x] === '#' || map[y][x] === '^' ) {
-				continue;
-			}
-
-			if( loop(x, y, start, map) ) {
-				sum++;
-			}
-
-		}
-		
-		if( y%10 === 0 ) {
-			console.log('testing', y+'/'+(map.length-1) );
-		}
-
-	}
-
-	return sum;
-}
-
-
-function loop( obstacleX, obstacleY, start, map ) {
+function loop( obstacleX, obstacleY, start, map, dir ) {
 
 	var curX = start[0],
 		curY = start[1];
-
-	var dir = 0; // 0: top, 1: right, 2: down, 3: left
 
 	var visitedMap = Array.from({ length: map.length }, function(){
 		return Array(map[0].length);
